25834648b9f88a0a85e8ca405bb3708bc67f9a12
ImDuong <shiroyasha699969@gmail.com>
2022-12-23 15:35:56 +0700

    add congratulations when game over

--- static/js/puzzle.js
+++ static/js/puzzle.js
@@ -3,26 +3,26 @@ var puzzleImageSrc = "../../assets/imgs/puzzle/"
 var rows = 6;
 var columns = 5;
 
+var nbIncorrectTiles = rows * columns;
+
 var currTile;
 var otherTile;
 
 var turns = 0;
 
 window.onload = function () {
-    //initialize the 5x5 board
+    //initialize the rows x columns board
     for (let r = 0; r < rows; r++) {
         for (let c = 0; c < columns; c++) {
             //<img>
             let tile = document.createElement("img");
             tile.src = puzzleImageSrc + "blank.jpg";
 
+            // store real 2D index, starting from 1
+            tile.original_piece = r * columns + c + 1
+
             //DRAG FUNCTIONALITY
-            tile.addEventListener("dragstart", dragStart); //click on image to drag
-            tile.addEventListener("dragover", dragOver);   //drag an image
-            tile.addEventListener("dragenter", dragEnter); //dragging an image into another one
-            tile.addEventListener("dragleave", dragLeave); //dragging an image away from another one
-            tile.addEventListener("drop", dragDrop);       //drop an image onto another one
-            tile.addEventListener("dragend", dragEnd);      //after you completed dragDrop
+            addEventListener(tile)
 
             document.getElementById("board").append(tile);
         }
@@ -31,8 +31,10 @@ window.onload = function () {
     //pieces: the best way to play this game is looking at the image name =))
     let pieces = [];
     for (let i = 1; i <= rows * columns; i++) {
-        pieces.push(i); //put 1 to 30 into the array (the 2D index)
+        pieces.push(i); //put 1 to (rows * columns) into the array (the 2D index, starting from 1)
     }
+
+    // shuffle the pieces
     pieces.reverse();
     for (let i = 0; i < pieces.length; i++) {
         let j = Math.floor(Math.random() * pieces.length);
@@ -43,23 +45,22 @@ window.onload = function () {
         pieces[j] = tmp;
     }
 
+    // initialize the randomized pieces
     for (let r = 0; r < rows; r++) {
         for (let c = 0; c < columns; c++) {
             let piece = pieces[r * columns + c]
             let randRow = Math.ceil(piece / columns)
             let randCol = ((piece - 1) % columns) + 1
-            
-            console.log(piece, "->", "row:", randRow, "col:", randCol);
+
+            // console.log(piece, "->", "row:", randRow, "col:", randCol);
             let tile = document.createElement("img");
             tile.src = puzzleImageSrc + "splitted/row-" + randRow + "-column-" + randCol + ".jpg";
 
+            // store real 2D index
+            tile.temp_piece = piece
+
             //DRAG FUNCTIONALITY
-            tile.addEventListener("dragstart", dragStart); //click on image to drag
-            tile.addEventListener("dragover", dragOver);   //drag an image
-            tile.addEventListener("dragenter", dragEnter); //dragging an image into another one
-            tile.addEventListener("dragleave", dragLeave); //dragging an image away from another one
-            tile.addEventListener("drop", dragDrop);       //drop an image onto another one
-            tile.addEventListener("dragend", dragEnd);      //after you completed dragDrop
+            addEventListener(tile)
 
             document.getElementById("pieces").append(tile);
         }
@@ -84,13 +85,27 @@ function dragLeave() {
 }
 
 function dragDrop() {
-    otherTile = this; //this refers to image that is being dropped on
+    otherTile = this; //this refers to the image that is being dropped on
 }
 
 function dragEnd() {
     if (currTile.src.includes("blank")) {
         return;
     }
+
+    if (currTile.temp_piece == otherTile.original_piece) {
+        otherTile.attached = true
+        nbIncorrectTiles--
+    } else if (currTile.attached && currTile.temp_piece != otherTile.original_piece) {
+        currTile.attached = false
+        nbIncorrectTiles++
+    }
+    otherTile.temp_piece = currTile.temp_piece
+
+    isWinnable()
+
+
+    // obj is not swapped, only swap the source
     let currImg = currTile.src;
     let otherImg = otherTile.src;
     currTile.src = otherImg;
@@ -98,4 +113,46 @@ function dragEnd() {
 
     turns += 100;
     document.getElementById("turns").innerText = turns;
+}
+
+function isWinnable() {
+    // console.log("Number of left over tiles:", nbIncorrectTiles);
+    if (nbIncorrectTiles == 0) {
+        // inform congratulations
+        console.log("Victoryyyy");
+        let congrats = document.getElementById("congrats");
+        congrats.style.display = "flex"
+
+        // lock all tiles in board
+        let board = document.getElementById("board")
+        for (let i = 0; i < board.childElementCount; i++) {
+            removeEventListener(board.children[i])
+        }
+
+        // lock all ties in pieces
+        let pieces = document.getElementById("pieces")
+        for (let i = 0; i < pieces.childElementCount; i++) {
+            removeEventListener(pieces.children[i])
+        }
+        return true
+    }
+    return false
+}
+
+function addEventListener(ele) {
+    ele.addEventListener("dragstart", dragStart); //click on image to drag
+    ele.addEventListener("dragover", dragOver);   //drag an image
+    ele.addEventListener("dragenter", dragEnter); //dragging an image into another one
+    ele.addEventListener("dragleave", dragLeave); //dragging an image away from another one
+    ele.addEventListener("drop", dragDrop);       //drop an image onto another one
+    ele.addEventListener("dragend", dragEnd);      //after you completed dragDrop
+}
+
+function removeEventListener(ele) {
+    ele.removeEventListener("dragstart", dragStart); //click on image to drag
+    ele.removeEventListener("dragover", dragOver);   //drag an image
+    ele.removeEventListener("dragenter", dragEnter); //dragging an image into another one
+    ele.removeEventListener("dragleave", dragLeave); //dragging an image away from another one
+    ele.removeEventListener("drop", dragDrop);       //drop an image onto another one
+    ele.removeEventListener("dragend", dragEnd);      //after you completed dragDrop
 }
